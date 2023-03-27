91f28c12f0a4fe5608b965d182e80d0937f26bd0
lucors <thesourcecode.max@yandex.ru>
2023-03-28 01:11:10 +0400

    Добавлены комнаты

---
+++ handler.js
@@ -0,0 +1,193 @@
+// [wsClient, ...]
+const clients = new Set();
+let incomingHandlers = []; //[{mode: string, func: function()},...]
+let flags = {
+  debug: false
+}
+const rooms = [
+  {title: "General", mems: new Set()},
+  {title: "Test", mems: new Set()},
+]
+
+
+
+//-----------------------------------------------------------------------------------
+// EXPORTS
+export function onConnect(client) {
+  console.log("Пользователь подключился");
+  direct(client, "NOTIFY", "Выберите канал");
+  client.on("message", (msg) => onMessage(client, msg));
+  client.on("close", (msg) => onClose(client));
+}
+//-----------------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------------
+// COMMON UTILS
+function direct(client, mode, data = undefined) {
+  let msg = [mode];
+  if (data) msg.push(data);
+  client.send(JSON.stringify(msg));
+}
+function totalBroadcast(mode, data) {
+  for (let client of clients) {
+    direct(client, mode, data);
+  }
+}
+function totalClientsNames() {
+  const list = [];
+  clients.forEach((client)=>{
+    list.push(client.who);
+  })
+  return list;
+}
+
+// ROOMS UTILS
+function checkRid(rid) {
+  if (rooms.length === 0) return false;
+  if (rid >= rooms.length || rid < 0) return false;
+  return true;
+}
+function roomMembersNames(rid) {
+  if (!checkRid(rid)) return;
+  const list = [];
+  rooms[rid].mems.forEach((member)=>{
+    list.push(member.who);
+  })
+  return list;
+}
+function roomsData() {
+  const list = [];
+  rooms.forEach((room, rid)=>{
+    list.push({title: room.title, rid: rid});
+  })
+  return list;
+}
+function roomBroadcast(rid, mode, data) {
+  if (!checkRid(rid)) return;
+  for (let member of rooms[rid].mems) {
+    direct(member, mode, data);
+  }
+}
+//-----------------------------------------------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------------
+// WSS HANDLERS
+function onClose(client){
+  let who = "Null";
+  clients.forEach((cl) => {
+    if (cl === client) {
+      autoleaveRoom(cl);
+      clients.delete(cl);
+      who = cl.who;
+      return;
+    }
+  });
+  console.log(`Пользователь отключился: ${who}`);
+  totalBroadcast("COUNT", clients.size);
+}
+
+function onMessage(client, raw){
+  try {
+      if (flags.debug) console.log(`Получено: ` + raw);
+      const message = JSON.parse(raw);
+      let _done = false;
+      incomingHandlers.forEach(handler => {
+          if (handler.mode == message[0]) {
+              handler.func(client, message);
+              _done = true;
+              return;
+          }
+      });
+      if (!_done) {
+          direct(client, "ERROR", `Неизвестный тип сообщения: ${message[0]}`);
+          console.error(`Ошибка обработки сообщения. mode: ${message[0]}`);
+      }
+  }
+  catch (error) {
+      direct(client, "ERROR", `Ошибка: ${error}`);
+      console.error("Ошибка: ", error);
+  }
+}
+
+incomingHandlers.push({
+  mode: "MSG",
+  func: function(client, message){
+    if (!clients.has(client)) return;
+    if (!("who" in client)) return;
+    if (!("rid" in client)) return;
+    if (!client.rid) return;
+    if (message.length < 2) return;
+    if (message[1].msg === "") return;
+
+    message[1].msg = message[1].msg.slice(0, 2000);
+    roomBroadcast(client.rid, "MSG", {who: client.who, msg: message[1].msg});
+  }
+});
+
+incomingHandlers.push({
+  mode: "AUTH",
+  func: function(client, message){
+    if (message.length < 2) return;
+    if (!message[1].who) return;
+
+    let who = message[1].who.slice(0, 50);
+    for (let cl of clients) {
+      if (cl.who === who) {
+        direct(client, "AUTH_FAIL", "Имя занято");
+        return;
+      }
+    }
+    client.who = who;
+    client.rid = undefined;
+    direct(client, "ROOMS", roomsData());
+    clients.add(client);
+    direct(client, "AUTH_OK");
+    totalBroadcast("COUNT", clients.size);
+  }
+});
+
+function leaveRoom(client, rid){
+  client.rid = undefined;
+  if (rooms[rid].mems.delete(client)) {
+    roomBroadcast(rid, "DEL_MEM", client.who);
+    roomBroadcast(rid, "ROOM_COUNT", rooms[rid].mems.size);
+  }
+}
+function autoleaveRoom(client){
+  if (!("rid" in client)) return;
+  if (!client.rid) return;
+  leaveRoom(client, client.rid);
+}
+function enterRoom(client, rid){
+  direct(client, "ROOM_CHANGE_OK", rid);
+  // direct(client, "NOTIFY", "Добро пожаловать в чат!");
+  direct(client, "MEMBERS", roomMembersNames(rid));
+  client.rid = rid;
+  rooms[rid].mems.add(client);
+  roomBroadcast(rid, "NEW_MEM", client.who);
+  roomBroadcast(rid, "ROOM_COUNT", rooms[rid].mems.size);
+}
+incomingHandlers.push({
+  mode: "ROOM_CHANGE",
+  func: function(client, message){
+    if (!client.who) return;
+    if (message.length < 2) return;
+
+    const rid = message[1];
+    if (!checkRid(rid)) {
+      direct(client, "ROOM_CHANGE_FAIL", "Комната не найдена");
+      return;
+    }
+    if (rooms[rid].mems.has(client)) {
+      direct(client, "ROOM_CHANGE_FAIL", "Вы уже в этой комнате");
+      return;
+    }
+    autoleaveRoom(client);
+    enterRoom(client, rid);
+  }
+});
+//-----------------------------------------------------------------------------------
--- main.js
+++ main.js
@@ -1,85 +1,8 @@
+import { onConnect } from './handler.js';
+import { WebSocketServer } from 'ws';
+
 const WebSocketPort = 9000;
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: WebSocketPort });
-
-// [wsClient, ...]
-const clients = new Set();
-
-function direct(client, mode, data = undefined) {
-  let msg = [mode];
-  if (data) msg.push(data);
-  client.send(JSON.stringify(msg));
-}
-function broadcast(mode, data) {
-  for (let client of clients) {
-    direct(client, mode, data);
-  }
-}
-function getClientsNameList() {
-  const list = [];
-  clients.forEach((client)=>{
-    list.push(client.who);
-  })
-  return list;
-}
-
-function onConnect(client) {
-  console.log("Пользователь подключился");
-  direct(client, "NOTIFY", "Добро пожаловать в чат!");
-
-  client.on("message", function (message) {
-    try {
-      message = JSON.parse(message);
-      switch (message[0]) {
-        case "MSG":
-          if (!clients.has(client)) return;
-          if (!("who" in client)) return;
-          if (message[1].msg === "") return;
-          message[1].msg = message[1].msg.slice(0, 2000);
-          broadcast("MSG", {who: client.who, msg: message[1].msg});
-          break;
-        case "NEWMEM":
-          if (!message[1].who) return;
-          let who = message[1].who.slice(0, 50);
-
-          for (let cl of clients) {
-            if (cl.who === who) {
-              direct(client, "NEWMEM_CHANGE_NICK", "Имя занято");
-              return;
-            }
-          }
-          client.who = who;
-          direct(client, "CLIENTS", getClientsNameList());
-          clients.add(client);
-          direct(client, "NEWMEM_OK");
-          broadcast("NEWMEM", who);
-          broadcast("COUNT", clients.size);
-          break;
-        default:
-          direct(client, "ERROR", `Неизвестный тип сообщения: ${message[0]}`);
-          console.error(`Неизвестный тип сообщения: ${message[0]}`);
-      }
-    }
-    catch (error) {
-      direct(client, "ERROR", `Ошибка: ${error}`);
-      console.error("Ошибка: ", error);
-    }
-  });
-
-  client.on("close", function () {
-    let who = "Null";
-    clients.forEach((cl) => {
-      if (cl === client) {
-        broadcast("DELMEM", cl.who);
-        clients.delete(cl);
-        who = cl.who;
-        return;
-      }
-    });
-    console.log(`Пользователь отключился: ${who}`);
-    broadcast("COUNT", clients.size);
-  });
-}
+const server = new WebSocketServer({ port: WebSocketPort });
 
 server.on('connection', onConnect);
 console.log(`Сервер запущен на порту: ${WebSocketPort}`);
